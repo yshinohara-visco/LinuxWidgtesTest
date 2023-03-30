c9dd8ad7f5b5f9a64e56b5fb6f2135bb4502679a
yshinohara-visco <89497265+yshinohara-visco@users.noreply.github.com>
2023-03-30 13:47:16 +0900

    作成

---
+++ Hello/main.cpp
@@ -0,0 +1,16 @@
+﻿
+#include <wx/wx.h>
+
+class myApp : public wxApp
+{
+public:
+	bool OnInit() override
+	{
+		wxFrame* frame = new wxFrame(NULL, wxID_ANY, "HelloWorld");
+		new wxStaticText( frame, -1, "Hello" );
+		frame->Show();
+		return true;
+	}
+};
+
+IMPLEMENT_APP( myApp )
